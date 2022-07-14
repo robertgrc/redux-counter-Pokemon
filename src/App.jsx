4362f5a37a4720c2aa37e7e26d4ca41f60feeed7
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-07-14 13:50:59 -0400

    256. Usar valores del store y despachar acciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { increment } from './store/slices/counter'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const{counter} = useSelector((state)=>state.counter)
+
+  const dispatch = useDispatch()
+
 
   return (
     <div className="App">
@@ -17,8 +22,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          count is: {counter}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
