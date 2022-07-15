dbe00eeaf8f9bc7d9fdc724e371cbda8bdb10ef4
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-07-15 11:41:38 -0400

    261Axios

--- src/store/slices/pokemon/pokemonSlice.js
+++ src/store/slices/pokemon/pokemonSlice.js
@@ -11,8 +11,10 @@ export const pokemonSlice = createSlice({
     startLoadingPokemons: (state /* action */) => {
       state.isLoading = true;
     },
-    setPokemons: (state /* action */) => {
-      console.log(action);
+    setPokemons: (state, action) => {
+      state.isLoading = false;
+      state.page = action.payload.page;
+      state.pokemons = action.payload.pokemons;
     },
   },
 });
